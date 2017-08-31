f3ed5ebb83f634223f6308651a9e9ba5e5cb521e
Jeff Reiner <theundeadghost@gmail.com>
2017-08-31 02:44:11 +0200

    :sparkles: Massive new rewrite!!! Wayyyyy pretty naow

---
+++ app.js
@@ -0,0 +1,33 @@
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('spike-js-standards')
+const pageId = require('spike-page-id')
+const sugarml = require('sugarml')
+const sugarss = require('sugarss')
+const env = process.env.SPIKE_ENV
+
+module.exports = {
+  devtool: 'source-map',
+  matchers: { html: '*(**/)*.html', css: '*(**/)*.sss' },
+  ignore: ['**/layout.html', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
+  reshape: htmlStandards({
+    parser: false,
+    locals: (ctx) => {
+      return {
+        pageId: pageId(ctx),
+        foo: 'bar'
+      }
+    },
+    minify: env === 'production'
+  }),
+  postcss: cssStandards({
+    path: [ './node_modules' ],
+    cssnano: {
+      mergeLonghand: false
+    },
+    parser: sugarss,
+    minify: env === 'production',
+    warnForDuplicates: env !== 'production'
+  }),
+  babel: jsStandards()
+}
