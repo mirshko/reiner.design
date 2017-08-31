7227e25e7758795084a4d2097bbf63be47a99375
Jeff Reiner <theundeadghost@gmail.com>
2017-08-31 02:30:50 +0200

    :fire: Cleanout

--- app.production.js
+++
@@ -1,8 +0,0 @@
-const {UglifyJsPlugin} = require('webpack').optimize
-
-module.exports = {
-  // disable source maps
-  devtool: false,
-  // webpack optimization and minfication plugins
-  plugins: [new UglifyJsPlugin()]
-}
