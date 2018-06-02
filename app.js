7e6be37df5d9d414aede9d8df6628e7317a2dae4
Jeff Reiner <theundeadghost@gmail.com>
2018-06-02 20:34:47 +0200

    :bug: Moving preload from toml into layout
    Also cleaning up spike app.js and prod w/ Prettier

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const htmlStandards = require('reshape-standard')
-const cssStandards = require('spike-css-standards')
-const jsStandards = require('spike-js-standards')
-const pageId = require('spike-page-id')
-const sugarml = require('sugarml')
-const sugarss = require('sugarss')
-const env = process.env.SPIKE_ENV
+const htmlStandards = require("reshape-standard");
+const cssStandards = require("spike-css-standards");
+const jsStandards = require("spike-js-standards");
+const pageId = require("spike-page-id");
+const sugarml = require("sugarml");
+const sugarss = require("sugarss");
+const env = process.env.SPIKE_ENV;
 
 module.exports = {
-  devtool: 'source-map',
-  matchers: { html: '*(**/)*.sgr', css: '*(**/)*.sss' },
-  ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
+  devtool: "source-map",
+  matchers: { html: "*(**/)*.sgr", css: "*(**/)*.sss" },
+  ignore: ["**/layout.sgr", "**/_*", "**/.*", "readme.md", "yarn.lock"],
   reshape: htmlStandards({
     parser: sugarml,
-    locals: (ctx) => {
+    locals: ctx => {
       return {
         pageId: pageId(ctx)
-      }
+      };
     },
-    minify: env === 'production'
+    minify: env === "production"
   }),
   postcss: cssStandards({
-    path: [ './node_modules' ],
+    path: ["./node_modules"],
     cssnano: {
       mergeLonghand: false
     },
     parser: sugarss,
-    minify: env === 'production',
-    warnForDuplicates: env !== 'production'
+    minify: env === "production",
+    warnForDuplicates: env !== "production"
   }),
   babel: jsStandards()
-}
+};
