a69067af892bfd8b6a7ae090a3377f2b68cc3fbe
Jeff Reiner <theundeadghost@gmail.com>
2018-07-05 20:46:28 +0200

    :recycle: Removing sugar syntax

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const htmlStandards = require("reshape-standard");
 const cssStandards = require("spike-css-standards");
 const jsStandards = require("spike-js-standards");
 const pageId = require("spike-page-id");
-const sugarml = require("sugarml");
-const sugarss = require("sugarss");
 const env = process.env.SPIKE_ENV;
 
 module.exports = {
   devtool: "source-map",
-  matchers: { html: "*(**/)*.html", css: "*(**/)*.sss" },
-  ignore: ["**/layout.html", "**/_*", "**/.*", "readme.md", "yarn.lock"],
+  ignore: [
+    "**/layout.html",
+    "**/_*",
+    "**/.*",
+    "readme.md",
+    "yarn.lock",
+    "package-lock.json"
+  ],
   reshape: htmlStandards({
-    parser: false,
     locals: ctx => {
       return {
         pageId: pageId(ctx),
@@ -25,7 +28,6 @@ module.exports = {
     cssnano: {
       mergeLonghand: false
     },
-    parser: sugarss,
     minify: env === "production",
     warnForDuplicates: env !== "production"
   }),
