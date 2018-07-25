d47700ca7cdbfdf17192f128afa48e1f49ef02b5
Jeff Reiner <theundeadghost@gmail.com>
2018-07-25 23:57:08 +0200

    Revert ":sparkles: Trying something out"
    This reverts commit e33d1b8e32b08cc570b8abb4691405d7ea055d13.

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,11 +6,3 @@ const styleElementBackground = elems =>
   elems.forEach(el => (el.style.backgroundColor = generateRandomPastelColor()));
 
 styleElementBackground(document.querySelectorAll(".section"));
-
-const netlifyEnvVarUrls = {
-  URL: process.env.URL,
-  DEPLOY_URL: process.env.DEPLOY_URL,
-  DEPLOY_PRIME_URL: process.env.DEPLOY_PRIME_URL
-};
-
-console.log(netlifyEnvVarUrls);
